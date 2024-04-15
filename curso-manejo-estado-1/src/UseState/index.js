3f81947a762b48990427b264ae21afb9590535ae
adan_due <ing.adanduenas@gmail.com>
2024-04-15 15:38:30 -0600

    Efectos con useEffect

--- curso-manejo-estado-1/src/UseState/index.js
+++ curso-manejo-estado-1/src/UseState/index.js
@@ -2,6 +2,24 @@ import React from "react";
 
 const UseState = ({ name }) => {
     const [error, setError] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+
+    React.useEffect(() => {
+        console.log('Empezando el efecto')
+
+        if(!!loading) {
+            setTimeout(() => {
+                console.log('Haciendo la validación')
+    
+                setLoading(false)
+    
+                console.log('Terminando la validación')
+            }, 3000);
+        }
+
+        console.log('Terminando el efecto')
+    }, [loading])
+
     return (
         <div>
             <h2>Eliminar {name}</h2>
@@ -9,9 +27,13 @@ const UseState = ({ name }) => {
             {error &&(
                 <p>Error: El código es incorrecto</p>
             )}
+            {loading &&(
+                <p>Cargando...</p>
+            )}
+            
             <input placeholder="Código de seguridad" />
             <button
-            onClick={() => setError(!error)}
+            onClick={() => setLoading(true)}
             >Comprobar</button>
         </div>
     )

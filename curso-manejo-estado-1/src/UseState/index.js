3265e7d4167e550a09922462f28cc31b69b4d0b1
adan_due <ing.adanduenas@gmail.com>
2024-04-19 14:50:46 -0600

    Estados independientes con useState

--- curso-manejo-estado-1/src/UseState/index.js
+++ curso-manejo-estado-1/src/UseState/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+const SECURITY_CODE = 'paradigma'
+
 const UseState = ({ name }) => {
+    const [value, setValue] = React.useState([])
     const [error, setError] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
+
 
     React.useEffect(() => {
         console.log('Empezando el efecto')
@@ -11,7 +15,13 @@ const UseState = ({ name }) => {
             setTimeout(() => {
                 console.log('Haciendo la validación')
     
-                setLoading(false)
+                if(value === SECURITY_CODE) {
+                    setLoading(false)
+                    setError(false)
+                } else {
+                    setLoading(false)
+                    setError(true)
+                }
     
                 console.log('Terminando la validación')
             }, 3000);
@@ -25,16 +35,25 @@ const UseState = ({ name }) => {
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
             {error &&(
+                <>
                 <p>Error: El código es incorrecto</p>
+                <p>Vuelve a intentarlo</p>
+                </>
             )}
             {loading &&(
                 <p>Cargando...</p>
             )}
             
-            <input placeholder="Código de seguridad" />
+            <input
+                placeholder="Código de seguridad"
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value)
+                }}
+                />
             <button
-            onClick={() => setLoading(true)}
-            >Comprobar</button>
+                onClick={() => setLoading(true)}
+            >   Comprobar</button>
         </div>
     )
 }
